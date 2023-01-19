9061ade7c23f51ef13ef897b7307ee447ef3da5e
waffe_x20 <argueta_eduardo@yahoo.de>
2023-01-18 19:28:57 -0600

    Improve color scheme

--- index.py
+++ index.py
@@ -12,6 +12,11 @@ folium.Choropleth(
     data=eco_footprints,
     columns=["Country/region", "Ecological footprint"],
     key_on="feature.properties.name",
+    fill_color="RdYlGn_r",
+    fill_opacity=0.8,
+    line_opacity=0.3,
+    nan_fill_color="white",
+    legend_name="Ecological footprint per capita",
 ).add_to(map)
 
 map.save("footprint.html")
